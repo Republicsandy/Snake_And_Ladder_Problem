7b74a949f8e59ed5287eef5a1498ed86b21306f4
Sandeep singh <70006683+Republicsandy@users.noreply.github.com>
2022-08-12 18:36:51 +0530

    git commit -m "[sandeep] merge Single Player file Added in to main"

--- SnakeAndLedderProblem/Program.cs
+++ SnakeAndLedderProblem/Program.cs
@@ -6,7 +6,24 @@ namespace SnakeAndLedderProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wellcome to the snake and ladder game");
+            Console.WriteLine("Welcome to Line Comparison Computation");
+            Console.WriteLine("1 for find the length of line");
+            Console.WriteLine("2 for check the line is equal or not");
+
+            int Option = Convert.ToInt32(Console.ReadLine());
+
+            switch (Option)
+            {
+
+                case 1:
+                    PlayerFirst qemrem = new PlayerFirst();
+                    qemrem.Player1();
+                    break;
+                case 2:
+                    PlayerSecond swapnum = new PlayerSecond();
+                    swapnum.Player2();
+                    break;
+            }
         }
     }
 }
