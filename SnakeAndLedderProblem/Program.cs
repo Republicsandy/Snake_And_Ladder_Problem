5823727096ab4058511aa7cc63e4ca42be43afcd
Republicsandy <sandy180388@gmail.com>
2022-08-12 17:15:01 +0530

    [sandeep] Snake and Ladder Game project main file added

---
+++ SnakeAndLedderProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeAndLedderProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to the snake and ladder game");
+        }
+    }
+}
